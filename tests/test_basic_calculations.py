549650430a87285272f142782250efd01828fe88
Adel Popelkova <adel.popelek@gmail.com>
2021-07-06 12:25:39 +0200

    Change `l` to `lens` in tests

--- tests/test_basic_calculations.py
+++ tests/test_basic_calculations.py
@@ -10,7 +10,7 @@ def test_setup():
     """
     Test one option how to create a lens.
     """
-    l = Lens(
+    lens = Lens(
         n1=1.0003,
         nL=1.5,
         n2=1.0003,
@@ -19,35 +19,35 @@ def test_setup():
         CT=3
     )
 
-    l.calculate()
+    lens.calculate()
 
-    assert l.D1 == (l.nL - l.n1) / l.r1
-    assert l.n1 == 1.0003
+    assert lens.D1 == (lens.nL - lens.n1) / lens.r1
+    assert lens.n1 == 1.0003
 
 
 def test_different_setup():
     """
     Test another option how to create a lens.
     """
-    l = Lens()
-    l.n1 = 1.0003
-    l.nL = 1.5
-    l.n2 = 1.0003
-    l.r1 = 50
-    l.r2 = -40
-    l.CT = 3
+    lens = Lens()
+    lens.n1 = 1.0003
+    lens.nL = 1.5
+    lens.n2 = 1.0003
+    lens.r1 = 50
+    lens.r2 = -40
+    lens.CT = 3
 
-    l.calculate()
+    lens.calculate()
 
-    assert l.D1 == (l.nL - l.n1) / l.r1
-    assert l.n1 == 1.0003
+    assert lens.D1 == (lens.nL - lens.n1) / lens.r1
+    assert lens.n1 == 1.0003
 
 
 def test_variables_before_calculation():
     """
     Test variables before calculation.
     """
-    l = Lens(
+    lens = Lens(
         n1=1.0003,
         nL=1.5,
         n2=1.0003,
@@ -56,84 +56,84 @@ def test_variables_before_calculation():
         CT=3
     )
 
-    assert l.n1 == 1.0003
-    assert l.D1 is None
+    assert lens.n1 == 1.0003
+    assert lens.D1 is None
 
 
 def test_calculated_variables():
     """
     Test if all variables are calculated correctly.
     """
-    l = Lens()
-    l.n1 = 1.0003
-    l.nL = 1.5
-    l.n2 = 1.0003
-    l.r1 = 50
-    l.r2 = -40
-    l.CT = 3
+    lens = Lens()
+    lens.n1 = 1.0003
+    lens.nL = 1.5
+    lens.n2 = 1.0003
+    lens.r1 = 50
+    lens.r2 = -40
+    lens.CT = 3
 
-    l.calculate()
+    lens.calculate()
 
     # math.isclose is used due to the floating point inaccuracy
-    assert isclose(l.D1, (l.nL - l.n1) / l.r1)
-    assert isclose(l.D2, (l.n2 - l.nL) / l.r2)
-    assert isclose(l.D, l.D1 + l.D2 - l.D1 * l.D2 * (l.CT / l.nL))
-    assert isclose(l.P1, (l.D2 / l.D) * (l.n1 / l.nL) * l.CT)
-    assert isclose(l.P2, -(l.D1 / l.D) * (l.n2 / l.nL) * l.CT)
-    assert isclose(l.f1, -l.n1 * l.EFL)
-    assert isclose(l.f2, l.n2 * l.EFL)
-    assert isclose(l.EFL, 1 / l.D)
-    assert isclose(l.FFL, l.f1 + l.P1)
-    assert isclose(l.BFL, l.f2 + l.P2)
-    assert isclose(l.NPS, l.f1 + l.f2)
+    assert isclose(lens.D1, (lens.nL - lens.n1) / lens.r1)
+    assert isclose(lens.D2, (lens.n2 - lens.nL) / lens.r2)
+    assert isclose(lens.D, lens.D1 + lens.D2 - lens.D1 * lens.D2 * (lens.CT / lens.nL))
+    assert isclose(lens.P1, (lens.D2 / lens.D) * (lens.n1 / lens.nL) * lens.CT)
+    assert isclose(lens.P2, -(lens.D1 / lens.D) * (lens.n2 / lens.nL) * lens.CT)
+    assert isclose(lens.f1, -lens.n1 * lens.EFL)
+    assert isclose(lens.f2, lens.n2 * lens.EFL)
+    assert isclose(lens.EFL, 1 / lens.D)
+    assert isclose(lens.FFL, lens.f1 + lens.P1)
+    assert isclose(lens.BFL, lens.f2 + lens.P2)
+    assert isclose(lens.NPS, lens.f1 + lens.f2)
 
 
 def test_variables_from_input():
     """
     Test if variables from input aren't overwritten.
     """
-    l = Lens()
-    l.n1 = 1.0003
-    l.nL = 1.5
-    l.n2 = 1.0003
-    l.r1 = 50
-    l.r2 = -40
-    l.CT = 3
+    lens = Lens()
+    lens.n1 = 1.0003
+    lens.nL = 1.5
+    lens.n2 = 1.0003
+    lens.r1 = 50
+    lens.r2 = -40
+    lens.CT = 3
 
-    l.calculate()
+    lens.calculate()
 
-    assert l.n1 == 1.0003
-    assert l.nL == 1.5
-    assert l.n2 == 1.0003
-    assert l.r1 == 50
-    assert l.r2 == -40
-    assert l.CT == 3
+    assert lens.n1 == 1.0003
+    assert lens.nL == 1.5
+    assert lens.n2 == 1.0003
+    assert lens.r1 == 50
+    assert lens.r2 == -40
+    assert lens.CT == 3
 
 
 def test_calculate_with_all_variables(capsys):
     """
     Test calculation with all variables given.
     """
-    l = Lens()
-    l.D1 = 0.00999400000000000
-    l.D2 = 0.0124925000000000
-    l.D = 0.0222367999100000
-    l.n1 = 1.0003
-    l.nL = 1.5
-    l.n2 = 1.0003
-    l.r1 = 50
-    l.r2 = -40
-    l.CT = 3
-    l.P1 = 1.12392500724714
-    l.P2 = -0.899140005797714
-    l.f1 = -44.9839906842963
-    l.f2 = 44.9839906842963
-    l.EFL = 44.9704995344359
-    l.FFL = 43.8600656770491
-    l.BFL = 44.0848506784985
-    l.NPS = 0
+    lens = Lens()
+    lens.D1 = 0.00999400000000000
+    lens.D2 = 0.0124925000000000
+    lens.D = 0.0222367999100000
+    lens.n1 = 1.0003
+    lens.nL = 1.5
+    lens.n2 = 1.0003
+    lens.r1 = 50
+    lens.r2 = -40
+    lens.CT = 3
+    lens.P1 = 1.12392500724714
+    lens.P2 = -0.899140005797714
+    lens.f1 = -44.9839906842963
+    lens.f2 = 44.9839906842963
+    lens.EFL = 44.9704995344359
+    lens.FFL = 43.8600656770491
+    lens.BFL = 44.0848506784985
+    lens.NPS = 0
 
-    l.calculate()
+    lens.calculate()
 
     captured = capsys.readouterr()
 
@@ -145,10 +145,10 @@ def test_calculate_with_no_variables():
     """
     Test calculation with no variables given.
     """
-    l = Lens()
+    lens = Lens()
 
     with pytest.raises(ValueError):
-        l.calculate()
+        lens.calculate()
 
 
 @pytest.mark.timeout(120)  # This test is kind of slow.
@@ -156,25 +156,25 @@ def test_not_enough_variables():
     """
     Test input with less variables.
     """
-    l = Lens()
-    l.n1 = 1.0003
-    l.n2 = 1.0003
-    l.r1 = 50
-    l.r2 = -40
-    l.CT = 3
+    lens = Lens()
+    lens.n1 = 1.0003
+    lens.n2 = 1.0003
+    lens.r1 = 50
+    lens.r2 = -40
+    lens.CT = 3
 
-    l.calculate()
+    lens.calculate()
 
     # At the moment testing only that the result is an expression,
     # currently not testing, that it is the correct expression.
-    assert isinstance(l.D1, sympy.core.expr.Expr)
-    assert isinstance(l.D2, sympy.core.expr.Expr)
-    assert isinstance(l.D, sympy.core.expr.Expr)
-    assert isinstance(l.P1, sympy.core.expr.Expr)
-    assert isinstance(l.P2, sympy.core.expr.Expr)
-    assert isinstance(l.f1, sympy.core.expr.Expr)
-    assert isinstance(l.f2, sympy.core.expr.Expr)
-    assert isinstance(l.EFL, sympy.core.expr.Expr)
-    assert isinstance(l.BFL, sympy.core.expr.Expr)
-    assert isinstance(l.FFL, sympy.core.expr.Expr)
-    assert l.NPS == 0
+    assert isinstance(lens.D1, sympy.core.expr.Expr)
+    assert isinstance(lens.D2, sympy.core.expr.Expr)
+    assert isinstance(lens.D, sympy.core.expr.Expr)
+    assert isinstance(lens.P1, sympy.core.expr.Expr)
+    assert isinstance(lens.P2, sympy.core.expr.Expr)
+    assert isinstance(lens.f1, sympy.core.expr.Expr)
+    assert isinstance(lens.f2, sympy.core.expr.Expr)
+    assert isinstance(lens.EFL, sympy.core.expr.Expr)
+    assert isinstance(lens.BFL, sympy.core.expr.Expr)
+    assert isinstance(lens.FFL, sympy.core.expr.Expr)
+    assert lens.NPS == 0
