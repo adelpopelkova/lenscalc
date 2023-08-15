e95bfe8b5cda64e6435d9d743300f5a8a3a97c61
Adel Popelkova <adel.popelek@gmail.com>
2023-08-15 11:37:59 +0200

    Use deepcopy in tests

--- tests/test_variable_combinations.py
+++ tests/test_variable_combinations.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 from math import isclose
 
 import pytest
@@ -55,24 +56,7 @@ def test_missing_passing(left_out):
     """
     Check result with combinations missing.
     """
-    lens = Lens()
-    lens.D1 = 0.00999400000000000
-    lens.D2 = 0.0124925000000000
-    lens.D = 0.0222367999100000
-    lens.n1 = 1.0003
-    lens.nL = 1.5
-    lens.n2 = 1.0003
-    lens.r1 = 50
-    lens.r2 = -40
-    lens.CT = 3
-    lens.P1 = 1.12392500724714
-    lens.P2 = -0.899140005797714
-    lens.f1 = -44.9839906842963
-    lens.f2 = 44.9839906842963
-    lens.EFL = 44.9704995344359
-    lens.FFL = -43.8600656770491
-    lens.BFL = 44.0848506784985
-    lens.NPS = 0
+    lens = deepcopy(ORIGINAL_LENS)
 
     for variable in left_out:
         setattr(lens, variable, None)
@@ -87,24 +71,7 @@ def test_missing_failing(left_out):
     """
     Test combinations which end up with ValueError.
     """
-    lens = Lens()
-    lens.D1 = 0.00999400000000000
-    lens.D2 = 0.0124925000000000
-    lens.D = 0.0222367999100000
-    lens.n1 = 1.0003
-    lens.nL = 1.5
-    lens.n2 = 1.0003
-    lens.r1 = 50
-    lens.r2 = -40
-    lens.CT = 3
-    lens.P1 = 1.12392500724714
-    lens.P2 = -0.899140005797714
-    lens.f1 = -44.9839906842963
-    lens.f2 = 44.9839906842963
-    lens.EFL = 44.9704995344359
-    lens.FFL = -43.8600656770491
-    lens.BFL = 44.0848506784985
-    lens.NPS = 0
+    lens = deepcopy(ORIGINAL_LENS)
 
     for variable in left_out:
         setattr(lens, variable, None)
