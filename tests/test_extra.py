69a90777f98b11a4ac90138546a7cfa95ca64b4b
Adel Popelkova <adel.popelek@gmail.com>
2023-08-15 10:46:36 +0200

    Add tests for copying lenses

--- tests/test_extra.py
+++ tests/test_extra.py
@@ -1,6 +1,9 @@
+from copy import copy, deepcopy
+
 from sympy.core.symbol import Symbol
 
 from lenscalc import Lens
+from test_variable_combinations import compare_two_lenses, ORIGINAL_LENS
 
 
 def test_extra_symbol():
@@ -32,3 +35,44 @@ def test_extra_string():
 
     assert isinstance(lens.extra_string_text, str)
     assert isinstance(lens.extra_string_number, str)
+
+
+def test_copy():
+    """
+    Test that it is posible to create a copy of a lens.
+
+    The copied lens is a unique object.
+    """
+    lens = copy(ORIGINAL_LENS)
+
+    assert lens is not ORIGINAL_LENS
+    assert compare_two_lenses(ORIGINAL_LENS, lens)
+
+    setattr(lens, "NPS", None)
+    assert lens.NPS != ORIGINAL_LENS.NPS
+
+
+def test_deepcopy():
+    """
+    Test that it is posible to create a deep copy of a lens.
+    """
+    lens = deepcopy(ORIGINAL_LENS)
+
+    assert lens is not ORIGINAL_LENS
+    assert compare_two_lenses(ORIGINAL_LENS, lens)
+
+    setattr(lens, "NPS", None)
+    assert lens.NPS != ORIGINAL_LENS.NPS
+
+
+def test_copy_extra():
+    """
+    Test copy of a lens with an extra attribute.
+    """
+    lens1 = Lens()
+    lens1.extra = "extra"
+
+    lens2 = copy(lens1)
+
+    assert lens1 is not lens2
+    assert lens1.extra == lens2.extra
